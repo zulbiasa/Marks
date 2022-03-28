0176fc3bfb5a059722bc417e95b1eea168a9f5ac
Administrator <Administrator@PSPKM2826-2254>
2022-03-28 13:02:54 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtGrade = New System.Windows.Forms.TextBox()
+        Me.txtPointer = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(70, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Your Grade"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(70, 138)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Your Pointer"
+        '
+        'txtGrade
+        '
+        Me.txtGrade.Location = New System.Drawing.Point(186, 66)
+        Me.txtGrade.Name = "txtGrade"
+        Me.txtGrade.Size = New System.Drawing.Size(100, 23)
+        Me.txtGrade.TabIndex = 2
+        '
+        'txtPointer
+        '
+        Me.txtPointer.Location = New System.Drawing.Point(186, 135)
+        Me.txtPointer.Name = "txtPointer"
+        Me.txtPointer.Size = New System.Drawing.Size(100, 23)
+        Me.txtPointer.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(195, 201)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Submit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(337, 273)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtPointer)
+        Me.Controls.Add(Me.txtGrade)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtGrade As TextBox
+    Friend WithEvents txtPointer As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Grade As Integer
+        Dim Pointer As Double
+
+        Grade = txtGrade.Text
+        Pointer = txtPointer.Text
+
+        Select Case
+        MessageBox.Show(Your Grade Is between)
+    End Sub
+End Class
